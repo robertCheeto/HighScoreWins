ed6350bc49455d2d87423b530cc668ba3ecea478
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-29 14:55:29 -0400

    * fixed up display message formatting for user

--- src/main/java/com/pluralsight/HighScoreWins.java
+++ src/main/java/com/pluralsight/HighScoreWins.java
@@ -15,20 +15,20 @@ public class HighScoreWins {
         String[] teamScores = boxScoreInfo[1].split("\\:");
 
         // below is a test statement to see if the boxScoreInfo[] was able to be broken into two separate arrays
-        System.out.println(teamNames[0] + " " + teamNames[1] + "<- team names | scores -> " + teamScores[0] + " " + teamScores[1]);
+        //System.out.println(teamNames[0] + " " + teamNames[1] + "<- team names | scores -> " + teamScores[0] + " " + teamScores[1]);
 
         //converting scores to Ints
         int homeTeamScore = Integer.parseInt(teamScores[0]);
         int awayTeamScore = Integer.parseInt(teamScores[1]);
 
         if (homeTeamScore > awayTeamScore) {
-            System.out.printf("Winner: %s", teamNames[0]);
+            System.out.printf("Winner: %s\nFinal Score: %d-%d", teamNames[0], homeTeamScore, awayTeamScore);
         } // end of if statement
         else if (homeTeamScore < awayTeamScore) {
-            System.out.printf("Winner: %s", teamNames[1]);
+            System.out.printf("Winner: %s\nFinal Score: %d-%d", teamNames[1], awayTeamScore, homeTeamScore);
         } // end of else if
         else {
-            System.out.printf("Both teams tied: %d", homeTeamScore);
+            System.out.printf("Both teams tied at: %d-%d", homeTeamScore, awayTeamScore);
         } // end of else
 
     } // end main

893d2f156741f9fc53f9a83cf086a2ea54d94a71
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-29 14:33:55 -0400

    * created souts explaining how the user should input text * created arrays for team names and box scores

--- src/main/java/com/pluralsight/HighScoreWins.java
+++ src/main/java/com/pluralsight/HighScoreWins.java
@@ -5,5 +5,16 @@ import java.util.Scanner;
 public class HighScoreWins {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-    }
+
+        System.out.println("Enter the two team names and score in the exact example format below.");
+        System.out.print("Home:Away|21:9 ");
+        String boxScoreInput = keyboard.nextLine();
+
+        String boxScoreInfo[] = boxScoreInput.split("\\|");
+        String teamNames[] = boxScoreInfo.split("\\:");
+        String teamScores[] = boxScoreInfo.split("\\:");
+
+        System.out.println(teamNames[0] + "<- team names | scores -> " + teamScores[0]);
+
+    } // end main
 }
